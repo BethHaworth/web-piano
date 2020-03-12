08f25aec7b216698cbff0f0a2d4b2f299055a695
BethHaworth <beth.haworth@focusrite.com>
2020-03-12 13:00:19 +0000

    Set up black and white keys within keyboard

--- my-piano/src/App.js
+++ my-piano/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Whitekey from './components/Whitekey';
+import Keyboard from './components/Keyboard';
+
 
 function App() {
   return (
     <div className="App">
-      <Whitekey />
+      < Keyboard />
     </div>
   );
 }
---
+++ my-piano/src/components/Blackkey.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Blackkey.css';
+
+class Blackkey extends React.Component {
+    render () {
+        return <button className= "blackkey">  </button>
+    }
+}
+
+export default Blackkey;
